4c0abd48ee99ef6d3e93316f738f9afb4216beab
jomarsv <47321363+jomarsv@users.noreply.github.com>
2026-03-18 14:32:27 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -4,16 +4,16 @@ import { getStorage } from "firebase/storage";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCSK8POTwUNM9FegMH9N9280HhrhRz6cJU",
+  apiKey: "AIzaSyCSK8POTwUNM9FegMH9N928OhHrhRz6cJU",
   authDomain: "sgtr-plataforma.firebaseapp.com",
   projectId: "sgtr-plataforma",
-  storageBucket: "sgtr-plataforma.appspot.com",
+  storageBucket: "sgtr-plataforma.firebasestorage.app",
   messagingSenderId: "883612105941",
   appId: "1:883612105941:web:6061d320adf00ed7c8365c"
 };
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app); 
+export const db = getFirestore(app);
 export const storage = getStorage(app);
 export const auth = getAuth(app);

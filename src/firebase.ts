657804325c1e8fb93645b3518b851027c6150fc3
jomarsv <47321363+jomarsv@users.noreply.github.com>
2026-03-18 12:26:25 -0300

    Update firebase.ts

--- src/firebase.ts
+++ src/firebase.ts
@@ -14,6 +14,6 @@ const firebaseConfig = {
 
 const app = initializeApp(firebaseConfig);
 
-export const db = getFirestore(app);
+export const db = getFirestore(app); 
 export const storage = getStorage(app);
 export const auth = getAuth(app);
